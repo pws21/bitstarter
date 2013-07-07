66215fdb6045d490cc2783ca3fbb15d0d73a80b4
Pavel Shiryaev <pws@front.ru>
2013-07-08 00:44:15 +0400

    remove header

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var f = fs.readFileSync('index.html', 'utf-8');
-  response.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+  //response.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
   response.send(f);
 });
 
